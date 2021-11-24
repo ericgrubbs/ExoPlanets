38b71b4b39aba84f702fe22c509986307af1fbab
Eric Grubbs <eric_grubbs@trekbikes.com>
2021-11-24 14:17:32 -0700

    fix compile error in  habitable planet method

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const habitablePlanets = [];
 
-function isHabitablePlanter(planet){
+function isHabitablePlanet(planet){
     return planet['koi_disposition'] == 'CONFIRMED'
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
     && planet['koi_prad'] < 1.6;
